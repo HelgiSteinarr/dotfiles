247e1081b7e815bf86d4542dc4e7a048710bd99f
HelgiSteinarr <helgi203@gmail.com>
2025-05-16 14:04:30 +0000

    moved package lists to new file, moved some yay packages from pac to yay array, fixed some references to ml4w

--- dothelgi/.config/dothelgi/install/install.sh
+++ dothelgi/.config/dothelgi/install/install.sh
@@ -1,56 +1,52 @@
 #!/bin/bash
 
-packages_pacman=(
-    "yay"
-    "wget"
-    "unzip"
-    "git"
-    "hyprland"
-    "waybar"
-    "rofi-wayland"
-    "alacritty"
-    "dunst"
-    "thunar"
-    "xdg-desktop-portal-hyprland"
-    "qt5-wayland"
-    "qt6-wayland"
-    "hyprpaper"
-    "hyprlock"
-    "hypridle"
-    "hyprshot"
-    "firefox"
-    "ttf-font-awesome"
-    "vim"
-    "fastfetch"
-    "ttf-fira-sans" 
-    "ttf-fira-code" 
-    "ttf-firacode-nerd"
-    "fuse2"
-    "gtk4"
-    "libadwaita"
-    "jq"
-    "python-gobject"
-    "xdotool"
-    "brightnessctl"
-    "networkmanager"
-    "wireplumber"
-    "nvim"
-    "pavucontrol"
-    "tlpui"
-    "zsh"
-    "s-tui"
-    "spotify"
-    "rustdesk"
-    "vivaldi"
-    "zen-browser-bin"
-    "starship"
-    "ttf-jetbrainsmono-nerd"
-)
+ensure_eof_newline() {
+    local file="$1"
+    [ -f "$file" ] || { echo "💀 BRO THAT AIN'T EVEN A FILE: $file" >&2; return 2; }
+    tail -c1 "$file" | read -r _ || echo >> "$file"
+}
 
-packages_yay=(
-    "wlogout"
-    "clipse"
-)
+ensure_eof_newline pkglist_pacman
+ensure_eof_newline pkglist_yay
 
-sudo pacman -S $packages_pacman --noconfirm
-yay -S $packages_yay --noconfirm
+packages_pacman=()
+packages_yay=()
+
+while IFS= read -r line; do
+    packages_pacman+=("${line}")
+done < "./pkglist_pacman"
+
+while IFS= read -r line; do
+    packages_yay+=("${line}")
+done < "./pkglist_yay"
+
+echo "${packages_pacman[@]}"
+echo "Lolooololol"
+echo "${packages_yay[@]}"
+
+sudo pacman -S "${packages_pacman[@]}" --noconfirm;
+# yay -S $packages_yay --noconfirm
+
+
+# TODO: The config file below should be put in /etc/greetd/config.toml
+#
+# [terminal]
+# # The VT to run the greeter on. Can be "next", "current" or a number
+# # designating the VT.
+# vt = 1
+# 
+# # The default session, also known as the greeter.
+# [default_session]
+# 
+# # `agreety` is the bundled agetty/login-lookalike. You can replace `/bin/sh`
+# # with whatever you want started, such as `sway`.
+# command = "agreety --cmd /bin/sh"
+# 
+# # The user to run the command as. The privileges this user must have depends
+# # on the greeter. A graphical greeter may for example require the user to be
+# # in the `video` group.
+# user = "greeter"
+# 
+# [initial_session]
+# command = "hyprland"
+# user = "helgi"
